5a3a876d1cdbe97a00f157fb159718dbcc75976e
DoXaisana <dodoxaisana123@gmail.com>
2024-12-27 10:05:51 +0700

    add rust files

---
+++ hello_world/target/debug/deps/hello_world-798874a9f846d720.d
@@ -0,0 +1,5 @@
+/Users/do_xaisana/Documents/CEIT/Year-4/Lesson/AIoT/git/hello_world/target/debug/deps/hello_world-798874a9f846d720: src/main.rs
+
+/Users/do_xaisana/Documents/CEIT/Year-4/Lesson/AIoT/git/hello_world/target/debug/deps/hello_world-798874a9f846d720.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/do_xaisana/Documents/CEIT/Year-4/Lesson/AIoT/git/hello_world/target/debug/hello_world: /Users/do_xaisana/Documents/CEIT/Year-4/Lesson/AIoT/git/hello_world/src/main.rs
